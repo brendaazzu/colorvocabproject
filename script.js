1b177fbff487de3a5f91bc045b2ae71aa83bfd53
Brenda Azzu <100886386+brendaazzu@users.noreply.github.com>
2023-10-17 22:44:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+/*                 var isSquareVisible0= true;
+
+                function changeColor0() {
+                    var square0 = document.getElementById("white");
+                    var button0 = document.getElementById("myButton0");
+                    if (isSquareVisible0) {
+                        square0.style.backgroundColor = "rgb(255, 255, 255)";
+                        square0.style.border = "2.5px solid black";
+                        button0.style.display = "none";
+                        isSquareVisible0 = false;
+                    } else {
+                        square0.style.display = "none";
+                        button0.style.display = "block";
+                        isSquareVisible0 = true;
+                    }
+                } */
